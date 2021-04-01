01365e56776f4c3e7cc9ea83496fd616a0d804f4
Davi Fernando Rosa <davir0514@gmail.com>
2021-04-01 13:33:45 -0300

    Arquivo index

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let paises = ["Brasil", "Suíça", "França", "Japão"];
+
+let contador = 0;
+
+while ( contador < paises.length ) {
+
+    console.log(paises[contador]);
+    contador++;
+
+}
+
+
+
+// A UTILIZAÇÃO DO WHILE E IMPORTANTISSIMA IMAGINA SE TIVESSE 100 PAISES ALI PARA IMPRIMIR 
+// E TER QUE FAZER ISSO UM A UM.
